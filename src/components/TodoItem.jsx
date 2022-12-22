9b3db4f300670d0a9cef9fb45de3aaa78c6be6c7
unknown <yejz0715@gmail.com>
2022-12-22 17:29:56 +0900

    Coponents_todoItem

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,53 @@
+import { React, useState } from "react";
+import styled from "styled-components";
+const TodoItem = ({
+  list,
+  setList,
+  id,
+  text,
+  regDate,
+  category,
+  isCompleted,
+}) => {
+  const [updateText, setUpdateText] = useState(text); //변경된 내용
+  const [readOnly, setReadOnly] = useState(true);
+  const [check, setCheck] = useState(isCompleted);
+  const handleChecked = () => {
+    setCheck((prev) => !prev);
+  };
+
+  const handleDeleteTodo = () => {
+    setList(list.filter((todo) => todo.id !== id));
+  };
+  const handleUpdateTodo = () => {
+    setReadOnly((prev) => !prev);
+  };
+
+  const handleText = (e) => {
+    setUpdateText(e.target.value);
+  };
+
+  return (
+    <Container>
+      <input type="checkbox" onChange={handleChecked} checked={check} />
+      id: {id} <br /> regDate : {regDate} <br />
+      text:
+      <input
+        type="text"
+        defaultValue={updateText}
+        readOnly={readOnly}
+        onChange={handleText}
+      ></input>
+      <br />
+      category: {category} <br />
+      <button onClick={handleDeleteTodo}>{!readOnly ? "취소" : "삭제"}</button>
+      <button onClick={handleUpdateTodo}>{!readOnly ? "확인" : "수정"}</button>
+    </Container>
+  );
+};
+export default TodoItem;
+
+const Container = styled.div`
+  background-color: pink;
+  margin-bottom: 12px;
+`;
