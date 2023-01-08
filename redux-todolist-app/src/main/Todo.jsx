901d41ff0b7ebf36681a589acaac8c02d291fb06
unknown <yejz0715@gmail.com>
2023-01-09 02:42:14 +0900

    ADD:todoComponents.jsx and style.js

---
+++ redux-todolist-app/src/main/Todo.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./todoStyle";
+import TodoInput from "../components/TodoInput/TodoInput";
+import TodoTemplate from "../components/TodoTemplate/TodoTemplate";
+import TodoList from "../components/TodoList/TodoList";
+import { todos } from "../data/dummy";
+import TodoHeader from "../components/TodoHeader/TodoHeader";
+import TodoCalender from "../components/TodoCalendar/TodoCalendar";
+
+const Todo = () => {
+  // 새로 만든 투두의 정보를 담을 객체
+  const [createdTodo, setCreatedTodo] = useState({});
+  const [list, setList] = useState(todos);
+  // handleAddTodo()를 실행
+
+  useEffect(() => {
+    if (!createdTodo.text) return;
+    handleAddTodo();
+  }, [createdTodo]);
+
+  //목록에 새로운 투두를 추가하는 함수
+  const handleAddTodo = () => {
+    // 배열에 새로운 투두 객체를 추가
+    // 이전 상태를 유지
+    setList([...list, createdTodo]);
+  };
+
+  const [date, setDate] = useState();
+  //체크
+  return (
+    <S.TodoContainer>
+      <S.TodoBlock>
+        <TodoCalender setDate={setDate} />
+      </S.TodoBlock>
+      <TodoTemplate>
+        <TodoHeader list={list} date={date} />
+
+        <TodoInput setCreatedTodo={setCreatedTodo} />
+        <TodoList setList={setList} list={list} />
+      </TodoTemplate>
+    </S.TodoContainer>
+  );
+};
+export default Todo;
---
+++ redux-todolist-app/src/main/todoStyle.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+export const TodoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+`;
+export const TodoBlock = styled.div`
+  width: 35rem;
+  height: 52rem;
+  border-radius: 0.5rem;
+  background-color: #fffff0;
+  margin-right: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// export const Calender = styled.img`
+//   width: 33rem;
+//   height: 44rem;
+
+//   border-radius: 0.7rem;
+// `;
