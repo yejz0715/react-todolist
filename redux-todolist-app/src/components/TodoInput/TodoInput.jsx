4a63738277d411a4a120c8a9285fd4b945f85bc6
unknown <yejz0715@gmail.com>
2023-01-17 00:18:14 +0900

    TodoInput_components_수정

--- redux-todolist-app/src/components/TodoInput/TodoInput.jsx
+++ redux-todolist-app/src/components/TodoInput/TodoInput.jsx
@@ -2,49 +2,103 @@ import React, { useState } from "react";
 import { useRef } from "react";
 import * as S from "./inputStyle";
 import PropsTypes from "prop-types";
+import RadioBox from "../TodoRadioBox/RadioBox";
+import EtcRadioButton from "../TodoRadioBox/EtcRadioButton";
 
-const TodoInput = ({ setCreatedTodo }) => {
+const TodoInput = ({ setCreatedTodo, nowDate }) => {
   // useRef를 사용해서 nextId값을 설정
   // usRef 값을 받아올 때, nextId.current
-  const [text, setText] = useState("");
   const nextId = useRef(4);
-  const [selected, setSelected] = useState("");
+  const [text, setText] = useState(""); //input_text
+  const [checked, setChecked] = useState("all"); //category_check
+  const [etcValue, setEtcValue] = useState("");
+
+  const handleChecked = (e) => {
+    //category_radio
+    setChecked(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleEtcValue = (e) => {
+    //etc_text
+    setEtcValue(e.target.value);
+  };
 
   // 새로운 투두를 만들어서 저장
   const handleCreateTodo = () => {
     // 새로운 투두 객체를 생성
-    setCreatedTodo({
-      id: nextId.current++,
-      text: text,
-      regDate: "",
-      category: selected,
-      isCompleted: false,
-    });
+    if (checked !== "etc") {
+      setCreatedTodo({
+        id: nextId.current++,
+        text: text,
+        regDate: nowDate,
+        category: checked,
+        isCompleted: false,
+      });
+    } else {
+      setCreatedTodo({
+        id: nextId.current++,
+        text: text,
+        regDate: nowDate,
+        category: `${checked}_${etcValue}`,
+        isCompleted: false,
+      });
+    }
     // 저장하고 인풋 초기화
     setText("");
   };
-
-  const handleSelect = (e) => {
-    setSelected(e.target.value);
-  };
-
   return (
-    <S.InputBox>
-      <S.Select onChange={handleSelect}>
-        <S.Option value="none">선택</S.Option>
-        <S.Option value="일상">일상</S.Option>
-        <S.Option value="공부">공부</S.Option>
-        <S.Option value="취미">취미</S.Option>
-        <S.Option value="기타">기타</S.Option>
-      </S.Select>
-      <S.Input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="할일을 입력해주세요!"
-      />
-      <S.InputButton onClick={handleCreateTodo}>추가</S.InputButton>
-    </S.InputBox>
+    <S.InputContainer>
+      <S.RadioBox>
+        <S.RadioLabel>카테고리</S.RadioLabel>
+        <RadioBox
+          name="category"
+          value="all"
+          text="전체"
+          defaultChecked={checked === "all"}
+          onChange={handleChecked}
+        />
+        <RadioBox
+          name="category"
+          value="daily"
+          text="일상"
+          defaultChecked={checked === "daily"}
+          onChange={handleChecked}
+        />
+        <RadioBox
+          name="category"
+          value="study"
+          text="공부"
+          defaultChecked={checked === "study"}
+          onChange={handleChecked}
+        />
+        <RadioBox
+          name="category"
+          value="hobby"
+          text="취미"
+          defaultChecked={checked === "hobby"}
+          onChange={handleChecked}
+        />
+        <EtcRadioButton
+          name="category"
+          value="etc"
+          text="기타"
+          defaultChecked={checked === "etc"}
+          onChange={handleChecked}
+          handleEtcValue={handleEtcValue}
+          etcValue={etcValue}
+        />
+      </S.RadioBox>
+      <S.InputBox>
+        <S.Input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="할일을 입력해주세요!"
+        />
+        <S.InputButton onClick={handleCreateTodo}>추가</S.InputButton>
+      </S.InputBox>
+    </S.InputContainer>
   );
 };
 
--- redux-todolist-app/src/components/TodoInput/inputStyle.js
+++ redux-todolist-app/src/components/TodoInput/inputStyle.js
@@ -1,11 +1,30 @@
 import styled from "styled-components";
 import { MdOutlineAddBox } from "react-icons/md";
+
+export const InputContainer = styled.div`
+  background-color: #e8f3d6;
+  padding: 1.5rem;
+  width: 55rem;
+`;
+export const RadioBox = styled.div`
+  margin-bottom: 2rem;
+  display: flex;
+`;
+export const RadioLabel = styled.label`
+  width: 8rem;
+  height: 2.5rem;
+  padding-top: 0.5rem;
+  margin-left: 0.5rem;
+  background-color: yellow;
+  border-radius: 1.5rem;
+  font-size: 1.8rem;
+  text-align: center;
+`;
 export const InputBox = styled.div`
   display: flex;
   justify-content: center;
-  padding: 1.5rem;
-  background-color: #e8f3d6;
   border-radius: 0.5rem;
+  margin-top: 5rem;
 `;
 export const Input = styled.input`
   width: 100%;
@@ -20,6 +39,3 @@ export const InputButton = styled(MdOutlineAddBox)`
   margin-left: 6px;
   cursor: pointer;
 `;
-export const Select = styled.select``;
-
-export const Option = styled.option``;
