2885894bf9ef4b25c17c7ce8e2e5f9a247fe76b6
unknown <yejz0715@gmail.com>
2023-01-18 13:55:33 +0900

    파일 삭제

--- react-todolist-app/src/components/TodoTemplate.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import * as S from "../style/templateStyle";
-const TodoTemplate = ({ children }) => {
-  return <S.TemplateBlock>{children}</S.TemplateBlock>;
-};
-export default TodoTemplate;
--- react-todolist-app/src/style/listStyle.js
+++
@@ -1,4 +0,0 @@
-import styled from "styled-components";
-export const ListBox = styled.div`
-  margin: 0 1.3rem;
-`;
