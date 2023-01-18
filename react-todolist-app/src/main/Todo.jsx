f3fc4e9a785cf62c15be893d1a77a40874f48d27
unknown <yejz0715@gmail.com>
2023-01-18 14:02:55 +0900

    todoPage

---
+++ react-todolist-app/src/main/Todo.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./todoStyle";
+import TodoInput from "../components/TodoInput/TodoInput";
+import TodoTemplate from "../components/TodoTemplate/TodoTemplate";
+import TodoList from "../components/TodoList/TodoList";
+import { todos } from "../data/dummy";
+import TodoCalendar from "../components/TodoCalendar/TodoCalendar";
+
+const Todo = () => {
+  // 새로 만든 투두의 정보를 담을 객체
+  const [createdTodo, setCreatedTodo] = useState({});
+  const [list, setList] = useState(todos);
+  const [date, setDate] = useState(new Date()); //날짜 저장할 상태
+  const nowDate = date.toLocaleDateString();
+  // handleAddTodo()를 실행
+  useEffect(() => {
+    if (!createdTodo.text) return;
+    handleAddTodo();
+  }, [createdTodo]);
+
+  //목록에 새로운 투두를 추가하는 함수
+  const handleAddTodo = () => {
+    // 배열에 새로운 투두 객체를 추가
+    // 이전 상태를 유지
+    setList([...list, createdTodo]);
+  };
+
+  return (
+    <S.TodoContainer>
+      <S.TodoBlock>
+        <S.HeaderTitle>TODOLIST</S.HeaderTitle>
+        <TodoCalendar setDate={setDate} date={date} />
+      </S.TodoBlock>
+
+      <TodoTemplate>
+        <S.NowDate>{nowDate}</S.NowDate>
+        {/*yyyy-MM-dd 형식으로*/}
+        <TodoInput setCreatedTodo={setCreatedTodo} nowDate={nowDate} />
+        <TodoList setList={setList} list={list} />
+      </TodoTemplate>
+    </S.TodoContainer>
+  );
+};
+export default Todo;
---
+++ react-todolist-app/src/main/todoStyle.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+export const TodoContainer = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  background-color: #f9f9f9;
+  padding: 1.7rem;
+  border-radius: 1rem;
+`;
+
+export const TodoBlock = styled.div`
+  padding-right: 2.5rem;
+`;
+export const HeaderTitle = styled.p`
+  padding: 3rem;
+  font-size: 7rem;
+`;
+
+export const NowDate = styled.p`
+  font-size: 3rem;
+  color: brown;
+  margin: 1.5rem;
+  padding: 0.5rem;
+`;
