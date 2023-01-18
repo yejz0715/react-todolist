f7d733f6bc3368f3540897acf6923c4796a32564
unknown <yejz0715@gmail.com>
2023-01-18 13:59:45 +0900

    selectBox_components

---
+++ react-todolist-app/src/components/SelectBox/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+export const SelectContainer = styled.div`
+  display: block;
+  position: relative;
+  background-color: #fefbf3;
+  width: 8rem;
+  font-size: 1.8rem;
+  margin-left: 1rem;
+  cursor: pointer;
+  height: 3rem;
+  padding-top: 1rem;
+  padding-bottom: 2.5rem;
+  text-align: center;
+  box-shadow: 0 0.3px 2px rgba(0, 0, 0, 0.2);
+`;
+export const SelectLabel = styled.label`
+  font-size: 1.8rem;
+`;
+export const OptionList = styled.ul`
+  width: 100%;
+  position: absolute;
+  overflow: hidden;
+  max-height: ${(props) => (props.isShow ? "none" : "0")};
+  background-color: white;
+  cursor: pointer;
+`;
+export const Option = styled.li`
+  border-bottom: 1px solid black;
+  padding-top: 0.7rem;
+`;
