393b52a42f901ebe4737ee08349e310406855db1
unknown <yejz0715@gmail.com>
2023-01-18 13:37:05 +0900

    radio_components 수정

--- redux-todolist-app/src/components/TodoRadioBox/style.js
+++ redux-todolist-app/src/components/TodoRadioBox/style.js
@@ -3,15 +3,19 @@ export const RadioLabel = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 1.8rem;
+  font-size: 2rem;
   width: 7rem;
   height: 2.5rem;
   padding-top: 0.5rem;
   margin-left: 0.5rem;
-  background-color: ${(props) => (props.defaultChecked ? "red" : "#e6e6e6")};
-  color: ${(props) => (props.defaultChecked ? "black" : "#777")};
+  background-color: ${(props) =>
+    props.defaultChecked ? "#ada2ff" : "#FFD4B2"};
+  color: ${(props) => (props.defaultChecked ? "#222" : "#222")};
   border-radius: 1.5rem;
   cursor: pointer;
+  &:hover {
+    background-color: #ada2ff;
+  }
 `;
 
 export const RadioInput = styled.input`
