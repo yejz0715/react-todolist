7392de8e4abf30b282888e5b61940962417e3887
unknown <yejz0715@gmail.com>
2023-01-18 13:36:41 +0900

    스타일 수정

--- redux-todolist-app/src/components/TodoItem/itemStyle.js
+++ redux-todolist-app/src/components/TodoItem/itemStyle.js
@@ -9,39 +9,40 @@ import {
 export const ItemContainer = styled.div`
   display: flex;
   align-items: center;
-  padding: 1rem;
-  background-color: ${(props) => (props.checked ? "#fcd8d4" : "red")};
+  justify-content: space-evenly;
+  padding: 0.5rem 0;
+  background-color: ${(props) => (props.checked ? "#65647C" : "#FFF1F1")};
   border-radius: 0.5rem;
   margin-bottom: 1rem;
-  line-height: 0.6rem;
+  line-height: 1rem;
 `;
 //체크
 export const Checked = styled(RiCheckboxFill)`
   cursor: pointer;
-  width: 1.4rem;
-  height: 1.4rem;
+  width: 2.8rem;
+  height: 2.8rem;
 `;
 
 export const UnChecked = styled(RiCheckboxBlankLine)`
   cursor: pointer;
-  width: 1.4rem;
-  height: 1.4rem;
+  width: 2.8rem;
+  height: 2.8rem;
 `;
 
 export const ItemBox = styled.div`
   display: flex;
   flex-direction: column;
-  width: 30rem;
-  margin: 0 1rem;
+  padding-top: 1rem;
   background-color: transparent;
+  font-size: 1.9rem;
 `;
 export const ItemInput = styled.input`
   border-bottom: 1px dashed gray;
-  width: 20rem;
+  width: 27rem;
   padding-top: 0.6rem;
   padding-bottom: 0;
-  line-height: 0.8rem;
   text-decoration: ${(props) => (props.checked ? "line-through" : "none")};
+  font-size: 1.6rem;
 `;
 export const ItemButtonBox = styled.div`
   display: flex;
@@ -49,32 +50,32 @@ export const ItemButtonBox = styled.div`
 //삭제
 export const Delete = styled(RiDeleteBin2Fill)`
   cursor: pointer;
-  width: 1.7rem;
-  height: 1.7rem;
+  width: 3.3rem;
+  height: 3.3rem;
   margin-right: 0.5rem;
   &:hover {
-    color: white;
+    color: #f190b7;
   }
 `;
 //수정
 export const Update = styled(RiDraftFill)`
   cursor: pointer;
-  width: 1.7rem;
-  height: 1.7rem;
+  width: 3.3rem;
+  height: 3.3rem;
   margin-right: 0.5rem;
 
   &:hover {
-    color: white;
+    color: #f190b7;
   }
 `;
 //저장취소
 export const IconBox = styled.div`
   cursor: pointer;
   padding: 0 0.15rem;
-  width: 1.6rem;
-  height: 1.6rem;
-  font-size: 0.8rem;
-  line-height: 1.9rem;
+  width: 3rem;
+  height: 3rem;
+  font-size: 1.6rem;
+  line-height: 3.3rem;
   background-color: #333333;
   color: #ffc0cb;
   border-radius: 0.3rem;
--- redux-todolist-app/src/components/TodoList/listStyle.js
+++ redux-todolist-app/src/components/TodoList/listStyle.js
@@ -1,4 +1,9 @@
 import styled from "styled-components";
 export const ListBox = styled.div`
   margin-top: 2rem;
+  background-color: #f9f9f9;
+`;
+export const SelectContainer = styled.div`
+  display: flex;
+  justify-content: end;
 `;
