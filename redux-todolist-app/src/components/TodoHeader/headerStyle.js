61e6436d00c2acd13ebedc30a8db133b7da8889d
unknown <yejz0715@gmail.com>
2023-01-18 13:32:19 +0900

    파일 삭제

--- redux-todolist-app/src/components/TodoHeader/headerStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderContainer = styled.div`
-  padding: 1.5rem;
-  display: flex;
-  flex-direction: column;
-`;
-
-export const RadioBox = styled.div``;
