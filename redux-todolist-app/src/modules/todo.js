a90b56a2f639f4417a09672461c8aed073be6724
unknown <yejz0715@gmail.com>
2023-01-19 17:41:40 +0900

    action, action_생성_함수, reducer 생성

--- redux-todolist-app/src/modules/todo.js
+++ redux-todolist-app/src/modules/todo.js
@@ -4,23 +4,17 @@ const UPDATE_TODO = "UPDATE_TODO";
 const DELETE_TODO = "DELETE_TODO";
 const TOGGLE_TODO = "TOGGLE_TODO";
 
-//default state(initState)=todos
-const initState = [
-  {
-    id: 1,
-    text: "교보문구 가기",
-    regDate: "2022-12-22",
-    category: "일상",
-    isCompleted: true,
-  },
-];
+//default state(initState)=todos, 초기상태
+const initState = {
+  todos: [],
+};
 
-let nextId = 1;
-//action 생성
+let nextId = 0;
+//액션생성함수
 export const addTodo = (todo) => {
   return {
     type: ADD_TODO,
-    todo: {
+    payload: {
       id: nextId++,
       text: todo.text,
       regDate: todo.regDate,
@@ -30,18 +24,66 @@ export const addTodo = (todo) => {
   };
 };
 
+export const updateTodo = (id, text) => {
+  return {
+    type: UPDATE_TODO,
+    payload: {
+      id: id,
+      text: text,
+    },
+  };
+};
+
+export const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    payload: { id: id },
+  };
+};
+
+export const toggleTodo = (id) => {
+  return {
+    type: TOGGLE_TODO,
+    payload: { id: id },
+  };
+};
+
 //todoReducer (상태,액션함수)
-export function todoReducer(state = initState, action) {
-  switch (action.type) {
+export function todoReducer(state = initState, { type, payload }) {
+  switch (type) {
     case ADD_TODO:
-      return state.concat(action.todo);
+      return {
+        ...state,
+        todos: state.todos.concat({
+          id: payload.id,
+          text: payload.text,
+          regDate: payload.regDate,
+          category: payload.category,
+          isCompleted: payload.isCompleted,
+        }),
+      };
     case UPDATE_TODO:
-      return;
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === payload.id ? { ...todo, text: payload.text } : todo
+        ),
+      };
     case DELETE_TODO:
-      return;
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== payload.id),
+      };
     case TOGGLE_TODO:
-      return;
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === payload.id
+            ? { ...todo, isCompleted: !todo.isCompleted }
+            : todo
+        ),
+      };
     default:
-      return state;
+      return { ...state };
   }
 }
