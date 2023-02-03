03b597335ef129ab4738f01aa082ee31197c072a
yejz0715 <yejz0715@gmail.com>
2023-02-03 23:57:31 +0900

    createSlice() 적용

--- redux-toolkit-todolist-app/src/modules/todo.js
+++ redux-toolkit-todolist-app/src/modules/todo.js
@@ -1,39 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { combineReducers, createSlice } from "@reduxjs/toolkit";
 
 const initState = {
   todos: [],
 };
-//createSlice(name, 초기값, reducer)
 let nextId = 0;
+
+//createSlice(name, initState, reducer)
 const todoSlice = createSlice({
   name: "todoReducer",
   initialState: initState,
   reducers: {
     addTodo: (state, { payload }) => {
-      state.push({
+      state.todos.push({
         id: nextId++,
         regDate: payload.regDate,
         text: payload.text,
         category: payload.category,
-        isCompleted: payload.isCompleted,
+        isCompleted: false,
       });
     },
-    updateTodo: (state, { payload }) =>
-      state.map((todo) =>
+
+    updateTodo: (state, { payload }) => {
+      state.todos.map((todo) =>
         todo.id === payload.id ? { ...todo, text: payload.text } : todo
-      ),
+      );
+    },
 
     deleteTodo: (state, { payload }) =>
-      state.filter((todo) => todo.id !== payload.id),
-    toggleTodo: (state, { payload }) =>
-      state.map((todo) =>
+      state.todos.filter((todo) => todo.id !== payload.id),
+    toggleTodo: (state, { payload }) => {
+      state.todos.map((todo) =>
         todo.id === payload.id
-          ? { ...todo, isCompleted: !todo.isCompleted }
+          ? { ...todo, isCompleted: !payload.isCompleted }
           : todo
-      ),
+      );
+    },
   },
 });
 export const { addTodo, updateTodo, deleteTodo, toggleTodo } =
   todoSlice.actions;
-
-export default todoSlice.reducer;
+let rootReducer = combineReducers({ reducer: todoSlice.reducer });
+export default rootReducer;
