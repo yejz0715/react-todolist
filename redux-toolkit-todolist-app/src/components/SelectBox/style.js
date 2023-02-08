2bfc2e1d727e160930121c7cdb6a3144873f065c
yejz0715 <yejz0715@gmail.com>
2023-02-09 00:05:16 +0900

    selectBox 스타일 수정

--- redux-toolkit-todolist-app/src/components/SelectBox/style.js
+++ redux-toolkit-todolist-app/src/components/SelectBox/style.js
@@ -2,33 +2,38 @@ import styled from "styled-components";
 export const SelectContainer = styled.div`
   display: block;
   position: relative;
-  background-color: #fefbf3;
   width: 100%;
   max-width: 8rem;
-  /* padding: 1rem; */
-  font-size: 1.8rem;
-  margin-left: 1rem;
-  cursor: pointer;
   height: 3rem;
-  padding-top: 1rem;
-  padding-bottom: 2.5rem;
+  padding: 1rem 0;
+  font-size: 1.5rem;
   text-align: center;
+  margin-left: 1rem;
+  background-color: #ffffff;
+  border: 1px solid gray;
   box-shadow: 0 0.3px 2px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
 `;
 export const SelectLabel = styled.label`
   width: 100%;
-  font-size: 1.8rem;
 `;
 export const OptionList = styled.ul`
-  width: 100%;
   position: absolute;
+  top: 2.95rem;
+  left: 0;
+  width: 100%;
   overflow: hidden;
   max-height: ${(props) => (props.isShow ? "none" : "0")};
-  background-color: white;
+  color: #666666;
+  border: 1px solid #b8b8b8;
+  border-top: none;
+  border-bottom: none;
+  background: #ffffff;
   cursor: pointer;
 `;
 export const Option = styled.li`
   width: 100%;
-  border-bottom: 1px solid black;
-  padding-top: 0.7rem;
+  border-bottom: 1px solid #b8b8b8;
+  padding: 0.7rem 0 0.3rem 0;
+  font-size: 1.4rem;
 `;
