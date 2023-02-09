763290082c2e3d01cc525fdaa8d5fe3c2ce5dbf7
yejz0715 <yejz0715@gmail.com>
2023-02-09 15:49:52 +0900

    스타일 수정

--- redux-toolkit-todolist-app/src/components/SelectBox/style.js
+++ redux-toolkit-todolist-app/src/components/SelectBox/style.js
@@ -3,14 +3,14 @@ export const SelectContainer = styled.div`
   display: block;
   position: relative;
   width: 100%;
-  max-width: 8rem;
+  max-width: 7rem;
   height: 3rem;
   padding: 1rem 0;
   font-size: 1.5rem;
   text-align: center;
   margin-left: 1rem;
   background-color: #ffffff;
-  border: 1px solid gray;
+  border: 1px solid #f0eeed;
   box-shadow: 0 0.3px 2px rgba(0, 0, 0, 0.2);
   cursor: pointer;
 `;
@@ -19,13 +19,13 @@ export const SelectLabel = styled.label`
 `;
 export const OptionList = styled.ul`
   position: absolute;
-  top: 2.95rem;
+  top: 2.9rem;
   left: 0;
   width: 100%;
   overflow: hidden;
   max-height: ${(props) => (props.isShow ? "none" : "0")};
   color: #666666;
-  border: 1px solid #b8b8b8;
+  border: 1px solid #f0eeed;
   border-top: none;
   border-bottom: none;
   background: #ffffff;
@@ -33,7 +33,7 @@ export const OptionList = styled.ul`
 `;
 export const Option = styled.li`
   width: 100%;
-  border-bottom: 1px solid #b8b8b8;
+  border-bottom: 1px solid #f0eeed;
   padding: 0.7rem 0 0.3rem 0;
   font-size: 1.4rem;
 `;
